4953f746db32b0d1fd049b2c89176a8dc99f6c2a
RSKPixel <166299316+RSKPixel@users.noreply.github.com>
2025-12-04 17:30:30 +0530

    upd

--- tools.py
+++ tools.py
@@ -1,14 +1,6 @@
 import numpy as np
 import pandas as pd
 
-import numpy as np
-import pandas as pd
-
-
-import numpy as np
-import pandas as pd
-
-
 def ddt(data: pd.DataFrame) -> pd.DataFrame:
     df = data.copy()
 
